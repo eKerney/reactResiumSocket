bec3b85e3b28e7e73a1fe877ab9d90945feefb46
eKerney <Eric.Kerney@airspacelink.com>
2022-04-12 09:31:35 -0400

    working react resium

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { Color } from "cesium";
+import { hot } from "react-hot-loader/root";
+import * as Cesium from 'cesium';
+//import React from "react";
+import { Viewer, Entity, PointGraphics, EntityDescription, GeoJsonDataSource, Scene, Globe, Camera} from "resium";
+import { Cartesian3, createWorldTerrain, onLoadAction } from "cesium";
+
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlMGY2Y2EwNy1lYjBjLTRlOTAtOTc4Yi01OGM4NTc5MTlhZWYiLCJpZCI6ODkzNTgsImlhdCI6MTY0OTcyNDM0OH0.d3owTfwWertUVKZyZ99sH-cWZJaPosgpJaotB5qmzJk';
+
+const cameraInit = Cartesian3.fromDegrees(-83.05, 42.33, 2000);
 
 function App() {
+  const initialView = {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Viewer full
+        baseLayerPicker={true}
+        infoBox={true}
+        homeButton={false}
+        timeline={false}
+        scene3DOnly={true}
+        shadows={true}
+        fullscreenButton={false}
+        animation={false}
+        
+    >
+      <Entity
+        polygon={{extrudedHeight: 1000}}
+      >
+      <GeoJsonDataSource data={"https://raw.githubusercontent.com/eKerney/reactMap-pointCloud/main/data/gpsSurfSamp2.json"} 
+        onLoad={d => {
+          console.log(d.entities.values[0].polygon);
+          //d.entities.values.map(d => d.polygon.extrudedHeight = 100)
+          d.entities.values.map(d => d.polygon.height = 100)
+          
+        }}
+        // fill={Color.AQUA}
+       
+        
+        
+      />
+      </Entity>
+      <Scene backgroundColor={Color.CORNFLOWERBLUE} />
+      <Globe />
+      <Camera 
+        position={cameraInit}
+
+        />
+    </Viewer>
   );
 }
 
