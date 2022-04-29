ef89b153d64062e2449dc2705156d95635b491c9
eKerney <Eric.Kerney@airspacelink.com>
2022-04-29 15:04:57 -0400

    MDOT Air Data Control

--- src/layer-control.js
+++ src/layer-control.js
@@ -14,6 +14,7 @@ function LayerControl(props) {
   const [GPS003, setGPS003 ] = useState(false);
   const [GPS050, setGPS050 ] = useState(false);
   const [GPS100, setGPS100 ] = useState(false);
+  const [MDOTairTraffic, setMDOTairTraffic ] = useState(false);
 
   const handleChange = (event) => {
     console.log(event.target.name);
@@ -22,7 +23,7 @@ function LayerControl(props) {
     event.target.name == 'GPS050' ? setGPS050(!GPS050) : setGPS050(GPS050);
     event.target.name == 'GPS100' ? setGPS100(!GPS100) : setGPS100(GPS100);
     event.target.name == 'BUILDINGS' ? setBuildings(!buildings) : setBuildings(buildings);
-    
+    event.target.name == 'MDOT' ? setMDOTairTraffic(!MDOTairTraffic) : setMDOTairTraffic(MDOTairTraffic);
     
   };
   
@@ -37,25 +38,30 @@ function LayerControl(props) {
         <hr style={{width: '800px', marginLeft: '-100px', marginTop: '26px'}}/>
         <br />
       <FormGroup>
-      <FormControlLabel control={<Checkbox name='BUILDINGS' checked={buildings} color="secondary" onChange={handleChange}/>} label="3D Buildings" />
+        <FormControlLabel control={<Checkbox name='BUILDINGS' checked={buildings} color="secondary" onChange={handleChange}/>} label="3D Buildings" />
+        <FormControlLabel control={<Checkbox name='MDOT' checked={MDOTairTraffic} color="secondary" onChange={handleChange}/>} label="MDOT Air Traffic Density" />
         <FormControlLabel control={<Checkbox name='GPS003' checked={GPS003} color="secondary" onChange={handleChange}/>} label="GPS Signal Strength 3 meters" />
         <FormControlLabel control={<Checkbox name='GPS050' checked={GPS050} color="secondary" onChange={handleChange}/>} label="GPS Signal Strength 50 meters " />
         <FormControlLabel control={<Checkbox name='GPS100' checked={GPS100} color="secondary" onChange={handleChange}/>} label="GPS Signal Strength 100 meters " />
       </FormGroup>
     </div>
-    
-       <GeoJsonDataSource data={"https://services5.arcgis.com/UDWrEU6HdWNYIRIV/ArcGIS/rest/services/MDOTdensityAll_29042022122250/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=&returnHiddenFields=false&returnGeometry=true&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&defaultSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson"} 
+    { MDOTairTraffic &&
+       <GeoJsonDataSource data={"https://raw.githubusercontent.com/eKerney/reactResiumSocket/main/src/data/MDOTairDensity.geojson"} 
        onLoad={d => {d.entities.values.map(d => {
-          //d.polygon.material = Color.NAVY.withAlpha(0.6);
-         //d.polygon.extrudedHeight = (d._properties.median_hgt * 2);
-         //const h = d._properties.median_hgt;
-         //d.polygon.material = h > (700) ? Color.NAVY.withAlpha(0.6) : h > (400) ? Color.TEAL.withAlpha(0.6) : h > (200) ? Color.LIGHTSEAGREEN.withAlpha(0.7) : 
-         //h > (100) ? Color.MEDIUMTURQUOISE.withAlpha(0.8) : h > (50) ? Color.PALETURQUOISE.withAlpha(0.8) : Color.ALICEBLUE.withAlpha(0.6); 
+          //d.polygon.material = new Color(.5,.5,.5, 0.5)
+          const h = (d._properties.level)*.3048;
+          const den = d._properties.density;
+          d.polygon.height = (h) - 100
+          d.polygon.extrudedHeight = (h) + 100;
+        //  d.polygon.material = h > (10000) ? Color.LAVENDER.withAlpha(0.2) : h > (5000) ? Color.LIGHTSTEELBLUE.withAlpha(0.2) : h > (3000) ? Color.PLUM.withAlpha(0.2) : 
+        //  h > (1000) ? Color.MEDIUMPURPLE.withAlpha(0.1) : h > (500) ? Color.BLUEVIOLET.withAlpha(0.2) : Color.DARKMAGENTA.withAlpha(0.3); 
+        d.polygon.material = den > (20) ? Color.DARKMAGENTA.withAlpha(0.3) : den > (10) ? Color.BLUEVIOLET.withAlpha(0.2) : den > (5) ? Color.MEDIUMPURPLE.withAlpha(0.1) : 
+        den > (1) ? Color.PLUM.withAlpha(0.2) : den > (.5) ? Color.LIGHTSTEELBLUE.withAlpha(0.2) : Color.LAVENDER.withAlpha(0.2); 
        })
        }}  
        stroke={Color.AQUA.withAlpha(0.0)}
      />
-    
+    }
     { buildings &&
        <GeoJsonDataSource data={"https://services5.arcgis.com/UDWrEU6HdWNYIRIV/ArcGIS/rest/services/buildingsClippedDet/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=-83.12%2C+42.23%2C+-83.04%2C+42.35&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token="} 
        onLoad={d => {d.entities.values.map(d => {
