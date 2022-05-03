1df8431ee133e54ef202deb41bf80681e6036571
eKerney <Eric.Kerney@airspacelink.com>
2022-05-03 13:38:58 -0400

    new data

--- src/layer-control.js
+++ src/layer-control.js
@@ -14,18 +14,18 @@ function LayerControl(props) {
   const [GPS050, setGPS050 ] = useState(false);
   const [GPS100, setGPS100 ] = useState(false);
   const [MDOTairTraffic, setMDOTairTraffic ] = useState(false);
-  const [testLayer, setTestLayer ] = useState(false);
+  // const [testLayer, setTestLayer ] = useState(false);
   const [GPSall, setGPSall ] = useState(false);
 
   const handleChange = (event) => {
     console.log(event.target.name);
     console.log(GPS003, GPS050, GPS100);
     event.target.name === 'GPS003' ? setGPS003(GPS003 => !GPS003) : setGPS003(GPS003 => GPS003);
-    event.target.name === 'GPS050' ? setGPS050(!GPS050) : setGPS050(GPS050);
-    event.target.name === 'GPS100' ? setGPS100(!GPS100) : setGPS100(GPS100);
+    event.target.name === 'GPS050' ? setGPS050(GPS050 => !GPS050) : setGPS050(GPS050 => GPS050);
+    event.target.name === 'GPS100' ? setGPS100(GPS100 => !GPS100) : setGPS100(GPS100 => GPS100);
     event.target.name === 'BUILDINGS' ? setBuildings(!buildings) : setBuildings(buildings);
     event.target.name === 'MDOT' ? setMDOTairTraffic(!MDOTairTraffic) : setMDOTairTraffic(MDOTairTraffic);
-    event.target.name === 'TESTLAYER' ? setTestLayer(!testLayer) : setTestLayer(testLayer);
+    //event.target.name === 'TESTLAYER' ? setTestLayer(!testLayer) : setTestLayer(testLayer);
     event.target.name === 'GPSALL' ? setGPSall(!GPSall) : setGPSall(GPSall);
   };
   
@@ -50,33 +50,18 @@ function LayerControl(props) {
         <br />
       <FormGroup>
         <FormControlLabel control={<Checkbox name='BUILDINGS' checked={buildings} color="secondary" onChange={handleChange}/>} label="3D Buildings" />
-        <FormControlLabel control={<Checkbox name='MDOT' checked={MDOTairTraffic} color="secondary" onChange={handleChange}/>} label="MDOT Air Traffic Density" />
+        <FormControlLabel control={<Checkbox name='MDOT' checked={MDOTairTraffic} color="secondary" onChange={handleChange}/>} label="MDOT AIR Traffic Density" />
+        {/* <FormControlLabel control={<Checkbox name='MDOT' checked={MDOTairTraffic} color="secondary" onChange={handleChange}/>} label="MDOT Air Traffic Density" /> */}
         <FormControlLabel control={<Checkbox name='GPS003' checked={GPS003} color="secondary" onChange={handleChange}/>} label="GPS Signal Strength 3 meters" />
         <FormControlLabel control={<Checkbox name='GPS050' checked={GPS050} color="secondary" onChange={handleChange}/>} label="GPS Signal Strength 50 meters " />
         <FormControlLabel control={<Checkbox name='GPS100' checked={GPS100} color="secondary" onChange={handleChange}/>} label="GPS Signal Strength 100 meters " />
-        <FormControlLabel control={<Checkbox name='TESTLAYER' checked={testLayer} color="secondary" onChange={handleChange}/>} label="MDOT AIR SELECTION" />
-        <FormControlLabel control={<Checkbox name='GPSALL' checked={GPSall} color="secondary" onChange={handleChange}/>} label="GPS ALL" />
+
+        {/* <FormControlLabel control={<Checkbox name='GPSALL' checked={GPSall} color="secondary" onChange={handleChange}/>} label="GPS ALL" /> */}
       </FormGroup>
     </div>
-    { testLayer &&
-       <GeoJsonDataSource data={"https://raw.githubusercontent.com/eKerney/dataStore/main/mdotDensitySelection.geojson"} 
-       onLoad={d => {d.entities.values.forEach(d => {
-          const h = (d._properties.level)*.3048;
-          const den = d._properties.density;
-          d.polygon.height = (h) - 100
-          d.polygon.extrudedHeight = (h) + 100;
-        //  d.polygon.material = h > (10000) ? Color.LAVENDER.withAlpha(0.2) : h > (5000) ? Color.LIGHTSTEELBLUE.withAlpha(0.2) : h > (3000) ? Color.PLUM.withAlpha(0.2) : 
-        //  h > (1000) ? Color.MEDIUMPURPLE.withAlpha(0.1) : h > (500) ? Color.BLUEVIOLET.withAlpha(0.2) : Color.DARKMAGENTA.withAlpha(0.3); 
-        d.polygon.material = den > (20) ? Color.DARKMAGENTA.withAlpha(0.4) : den > (10) ? Color.BLUEVIOLET.withAlpha(0.4) : 
-        den > (5) ? Color.MEDIUMPURPLE.withAlpha(0.3) : den > (1) ? Color.PLUM.withAlpha(0.15) : den > (.5) ? 
-        Color.LIGHTSTEELBLUE.withAlpha(0.1) : Color.LAVENDER.withAlpha(0.1); 
-       })
-       }}  
-       stroke={Color.AQUA.withAlpha(0.0)}
-     />
-    }
+
     { MDOTairTraffic &&
-       <GeoJsonDataSource data={"https://raw.githubusercontent.com/eKerney/reactResiumSocket/main/src/data/MDOTairDensity.geojson"} 
+       <GeoJsonDataSource data={"https://raw.githubusercontent.com/eKerney/dataStore/main/mdotDensitySelection.geojson"} 
        onLoad={d => {d.entities.values.forEach(d => {
           const h = (d._properties.level)*.3048;
           const den = d._properties.density;
@@ -90,7 +75,7 @@ function LayerControl(props) {
      />
     }
     { buildings &&
-       <GeoJsonDataSource data={"https://services5.arcgis.com/UDWrEU6HdWNYIRIV/ArcGIS/rest/services/buildingsClippedDet/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=-83.12%2C+42.23%2C+-83.04%2C+42.35&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token="} 
+       <GeoJsonDataSource data={"https://services5.arcgis.com/UDWrEU6HdWNYIRIV/ArcGIS/rest/services/buildingsClippedDet/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=-83.12%2C+42.23%2C+-83.04%2C+42.37&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token="} 
        onLoad={d => {d.entities.values.forEach(d => {
          d.polygon.extrudedHeight = (d._properties.median_hgt * 2);
          const h = d._properties.median_hgt;
@@ -101,7 +86,7 @@ function LayerControl(props) {
        stroke={Color.AQUA.withAlpha(0.0)}
      />
     }
-    { GPSall && 
+    { GPS003 && 
       <GeoJsonDataSource data={"https://raw.githubusercontent.com/eKerney/dataStore/main/spirent_gps_202204.geojson"} 
           onLoad={d => {d.entities.values.forEach(d => {
             d.polygon.height = 0;
@@ -110,45 +95,33 @@ function LayerControl(props) {
             console.log(d._properties._dop_worst_agl003_202204._value);
             d.polygon.material = d._properties._dop_worst_agl003_202204._value < 1 ?
                                   pdopColBad[d._properties._dop_worst_agl003_202204._value] :
-                                  pdopColBad[d._properties._dop_worst_agl003_202204._value] ;
-                                  
-                      
+                                  pdopColBad[d._properties._dop_worst_agl003_202204._value] ;                      
             })
           }}
           stroke={Color.GRAY.withAlpha(0.0)}
       /> 
     }
-    { GPS003 && 
-      <GeoJsonDataSource data={"https://raw.githubusercontent.com/eKerney/reactResium/main/src/data/agl_003_h3_11_worst.geojson"} 
-          onLoad={d => {d.entities.values.forEach(d => {
-            d.polygon.height = 0;
-            d.polygon.extrudedHeight = 50;
-            d.polygon.material = d._properties._dop_worst._value < 1 ? pdopColor[d._properties._dop_worst._value].withAlpha(0.0) :
-                                pdopColor[d._properties._dop_worst._value].withAlpha(0.2);        
-            })
-          }}
-          stroke={Color.GRAY.withAlpha(0.1)}
-      /> 
-    }
     { GPS050 &&
-      <GeoJsonDataSource data={"https://raw.githubusercontent.com/eKerney/reactResium/main/src/data/agl_005_h3_11_worst.geojson"} 
+      <GeoJsonDataSource data={"https://raw.githubusercontent.com/eKerney/dataStore/main/spirent_gps_202204.geojson"}
           onLoad={d => {d.entities.values.forEach(d => {
             d.polygon.height = 100;
             d.polygon.extrudedHeight = 150;
-            d.polygon.material = d._properties._dop_worst._value < 1 ? pdopColor[d._properties._dop_worst._value].withAlpha(0.0) :
-                                 pdopColor[d._properties._dop_worst._value].withAlpha(0.2);        
+            d.polygon.material = d._properties._dop_worst_agl050_202204._value < 1 ?
+                                  pdopColBad[d._properties._dop_worst_agl050_202204._value] :
+                                  pdopColBad[d._properties._dop_worst_agl050_202204._value] ;
             })
           }}
           stroke={Color.GRAY.withAlpha(0.0)}
         />
     }
     { GPS100 && 
-      <GeoJsonDataSource data={"https://raw.githubusercontent.com/eKerney/reactResium/main/src/data/agl_100_h3_11_worst.geojson"} 
+      <GeoJsonDataSource data={"https://raw.githubusercontent.com/eKerney/dataStore/main/spirent_gps_202204.geojson"}
           onLoad={d => {d.entities.values.forEach(d => {
             d.polygon.height = 200;
             d.polygon.extrudedHeight = 250;
-            d.polygon.material = d._properties._dop_worst._value < 1 ? pdopColor[d._properties._dop_worst._value].withAlpha(0.0) :
-                                 pdopColor[d._properties._dop_worst._value].withAlpha(0.2);        
+            d.polygon.material = d._properties._dop_worst_agl100_202204._value < 1 ?
+                                  pdopColBad[d._properties._dop_worst_agl100_202204._value] :
+                                  pdopColBad[d._properties._dop_worst_agl100_202204._value] ;      
             })
           }}
           stroke={Color.GRAY.withAlpha(0.0)}
